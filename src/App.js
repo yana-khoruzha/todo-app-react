fe55ad89cead21b8111fa1c52307fad2418fbc34
Yana Khoruzha <i.am.kitiket@gmail.com>
2023-04-05 01:39:48 +0300

    refactoring Todo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 import { TodoForm } from './components/Todos/TodoForm';
 import { TodoList } from './components/Todos/TodoList';
@@ -6,10 +7,15 @@ import { TodoList } from './components/Todos/TodoList';
 function App() {
   const [todos, setTodos] = useState([]);
   const addTodoHandler = (text) => {
-    setTodos([...todos, text]);
+    const newTodo = {
+      text: text,
+      isCompleted: false,
+      id: uuidv4(),
+    }
+    setTodos([...todos, newTodo]);
   };
-  const deleteTodoHandler = (index) => {
-    setTodos(todos.filter((_, idx) => idx !== index));
+  const deleteTodoHandler = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
   };
 
   return (
