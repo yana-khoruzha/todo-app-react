a1f9eb37000e5e594115b709e4f9e33be6cf37de
Yana Khoruzha <i.am.kitiket@gmail.com>
2023-04-05 01:01:43 +0300

    add todolist, delete todo by doubleclick

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { useState } from 'react';
 import './App.css';
+import { TodoForm } from './components/Todos/TodoForm';
+import { TodoList } from './components/Todos/TodoList';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const addTodoHandler = (text) => {
+    setTodos([...todos, text]);
+  };
+  const deleteTodoHandler = (index) => {
+    setTodos(todos.filter((_, idx) => idx !== index));
+  };
+
   return (
     <div className="App">
-      {/* #282C34 */}
+      <h1>Todo App</h1>
+      <TodoForm addTodo={addTodoHandler} />
+      <TodoList todos={todos} deleteTodo = {deleteTodoHandler} />
     </div>
   );
 }
