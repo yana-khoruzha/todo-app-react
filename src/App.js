f1e9764167cc4f32426d2184d27054dc8ac0d975
Yana Khoruzha <i.am.kitiket@gmail.com>
2023-04-10 20:04:00 +0300

    feat: add functionality to the buttons

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 import { TodoForm } from './components/Todos/TodoForm';
 import { TodoList } from './components/Todos/TodoList';
+import { TodosActions } from './components/Todos/TodosActions';
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -11,18 +12,45 @@ function App() {
       text: text,
       isCompleted: false,
       id: uuidv4(),
-    }
+    };
     setTodos([...todos, newTodo]);
   };
+
   const deleteTodoHandler = (id) => {
     setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const toggleTodoHandler = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        return todo.id === id
+          ? { ...todo, isCompleted: !todo.isCompleted }
+          : { ...todo };
+      })
+    );
+  };
+
+  const resetTodosHandler = () => {
+    setTodos([]);
+  };
+
+  const deleteCompletedTodosHandler = () => {
+    setTodos(todos.filter((todo) => !todo.isCompleted));
   };
 
   return (
     <div className="App">
       <h1>Todo App</h1>
       <TodoForm addTodo={addTodoHandler} />
-      <TodoList todos={todos} deleteTodo = {deleteTodoHandler} />
+      <TodosActions
+        resetTodos={resetTodosHandler}
+        deleteCompletedTodos={deleteCompletedTodosHandler}
+      />
+      <TodoList
+        todos={todos}
+        deleteTodo={deleteTodoHandler}
+        toggleTodo={toggleTodoHandler}
+      />
     </div>
   );
 }
